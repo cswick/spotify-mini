fb85c5360769de65451fe4d65687617230eb8938
ryan nauman <ryanjnauman@gmail.com>
2016-08-09 21:54:05 -0400

    long polling

--- app/helpers/spotify.js
+++ app/helpers/spotify.js
@@ -1,59 +1,56 @@
 const async = require('async');
-const request = require('request');
-
-// curl "https://zfvzhkzonj.spotilocal.com:4370/service/version.json?service=remote
-// &cors=&ref=https"%"3A"%"2F"%"2Fopen.spotify.com"%"2Falbum"%"2F3WjDB3j2yZL4g0hBGHF04i"
-// -H "Pragma: no-cache"
-// -H "Origin: https://open.spotify.com"
-// -H "Accept-Encoding: gzip, deflate, sdch, br"
-// -H "Accept-Language: en-US,en;q=0.8"
-// -H "Accept: */*"
-// -H "Cache-Control: no-cache"
-// -H "Referer: https://open.spotify.com/album/3WjDB3j2yZL4g0hBGHF04i"
+const request = require('request-promise');
+const P = require('bluebird');
 
 let _port = null;
 let _oauth = null;
+let _csrf = null;
+
+request.debug = true;
 
 function getAuth() {
-  request.get('http://open.spotify.com/token', function(err, res, body) {
-    _oauth = body.t;
-  });
+ return request.get({ url: 'http://open.spotify.com/token', json: true }, function(err, res, body) {
+   _oauth = body.t;
+ }).promise();
 }
 
-function getLocal(options) {
-  //url, callback = null, oauth = false, cfid = null, wait = -1, port = null
-  Object.assign({  }, {})
-  let port = options.port || _port;
-  let baseUrl = `https://mini.spotilocal.com:${port}${options.url}`;
-  let opts = {
-    url :  url,
-    json : true,
-    rejectUnauthorized: false
-  };
-  request(opts, options.callback);
+function getLocal(requestOpts, localOpts, callback) {
+  callback = callback || function () {};
+  requestOpts = Object.assign({json: true, rejectUnauthorized: false}, requestOpts);
+  requestOpts.headers = Object.assign({ 'Origin': 'https://open.spotify.com' }, requestOpts.headers);
+  localOpts = Object.assign({ oauth: false, cfid: false, wait: false, port: null }, localOpts);
+  localOpts.port = localOpts.port || _port;
+  requestOpts.url = `https://mini.spotilocal.com:${localOpts.port}${requestOpts.url}?service=remote&cors=&ref=https://open.spotify.com`;
+  if (localOpts.oauth) requestOpts.url += `&oauth=${_oauth}`;
+  if (localOpts.csrf) requestOpts.url += `&csrf=${_csrf}`;
+  if (localOpts.wait) requestOpts.url += '&returnon=login,logout,play,pause,error,ap&returnafter=60';
+  return request(requestOpts, callback).promise();
 }
 
 function probePorts() {
-  let ports = [4370, 4371, 4372, 4373, 4374, 4375, 4376, 4377, 4378, 4379];
-
-  let httpGet = function(port, callback) {
-    getLocal({
-      url: '/service/version.json?service=remote&cors=&ref=https://open.spotify.com',
-      port: port,
-      callback: (err, res, body) => {
-        console.log(`got it, port ${port}`);
-      })
-    });
-  }
-
-  async.map(ports, httpGet, (err, res, body) => {
-    console.log('done polling ports');
+  return new P((resolve, reject) => {
+    let ports = [4370, 4371, 4372, 4373, 4374, 4375, 4376, 4377, 4378, 4379];
+    let httpGet = function(port, cb) {
+     return getLocal({ url: '/service/version.json' }, { port: port }, (err, res, body) => {
+       if (!err && res.statusCode >= 200 && res.statusCode < 300) {
+         _port = port;
+         resolve();
+       }
+     }).catch(() => {});
+    }
+    async.map(ports, httpGet);
   });
 }
 
 function csrf() {
-  simplecsrf/token.json
+  return getLocal({ url: '/simplecsrf/token.json' }, null, (err, res, body) => {
+    _csrf = body.token;
+  });
 }
 
-getAuth();
-probePorts();
+P.join(getAuth(), probePorts(), () => {
+  csrf().then(() => {
+    console.log(`${_port}::${_oauth}::${_csrf}`);
+    getLocal({ url: '/remote/status.json' }, { csrf: true, oauth: true, wait: true }, (err, res, body) => { console.log(body); });
+  });
+});
